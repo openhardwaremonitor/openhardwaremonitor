9bf2340e9514ab3936b7b528f71963c2f152a15e
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2010-02-03 22:02:58 +0000

    Release version 0.1.12. Added error handling for PerformanceCounters (CPU load sensor).

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.11.0")]
-[assembly: AssemblyFileVersion("0.1.11.0")]
+[assembly: AssemblyVersion("0.1.12.0")]
+[assembly: AssemblyFileVersion("0.1.12.0")]
