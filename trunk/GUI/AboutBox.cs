ec99ab601b0d6e94f537812ac2aa1050c84f879e
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2010-02-27 20:26:05 +0000

    Fixed the version number in the AboutBox.

--- trunk/GUI/AboutBox.cs
+++ trunk/GUI/AboutBox.cs
@@ -47,11 +47,11 @@ namespace OpenHardwareMonitor.GUI {
     public AboutBox() {
       InitializeComponent();
       this.Font = SystemFonts.MessageBoxFont;
-
+      this.label3.Text = "Version " + 
+        System.Windows.Forms.Application.ProductVersion + " Beta";
       linkLabel.Links.Remove(linkLabel.Links[0]);
       linkLabel.Links.Add(0, linkLabel.Text.Length,
         "http://openhardwaremonitor.org");
-
     }
 
     private void linkLabel1_LinkClicked(object sender, 
