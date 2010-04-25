23a403dd7ca9211e4bc44b0d3c11a9e220bf40f1
Michael Möller <mmoeller@openhardwaremonitor.org>
2010-04-25 12:03:43 +0000

    Created a new wrapper for the Task Scheduler to fix some problem where TaskScheduler.Connect would throw an ArgumentException.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.29.0")]
-[assembly: AssemblyFileVersion("0.1.29.0")]
+[assembly: AssemblyVersion("0.1.30.0")]
+[assembly: AssemblyFileVersion("0.1.30.0")]
