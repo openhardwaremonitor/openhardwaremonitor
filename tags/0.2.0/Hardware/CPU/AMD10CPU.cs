bada908f11e4f585d287b66b49e677cb104a86f1
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2010-10-02 18:00:52 +0000

    Release version 0.2.0

---
+++ tags/0.2.0/Hardware/CPU/AMD10CPU.cs
@@ -0,0 +1,247 @@
+﻿/*
+  
+  Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+  The contents of this file are subject to the Mozilla Public License Version
+  1.1 (the "License"); you may not use this file except in compliance with
+  the License. You may obtain a copy of the License at
+ 
+  http://www.mozilla.org/MPL/
+
+  Software distributed under the License is distributed on an "AS IS" basis,
+  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+  for the specific language governing rights and limitations under the License.
+
+  The Original Code is the Open Hardware Monitor code.
+
+  The Initial Developer of the Original Code is 
+  Michael Möller <m.moeller@gmx.ch>.
+  Portions created by the Initial Developer are Copyright (C) 2009-2010
+  the Initial Developer. All Rights Reserved.
+
+  Contributor(s):
+
+  Alternatively, the contents of this file may be used under the terms of
+  either the GNU General Public License Version 2 or later (the "GPL"), or
+  the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+  in which case the provisions of the GPL or the LGPL are applicable instead
+  of those above. If you wish to allow use of your version of this file only
+  under the terms of either the GPL or the LGPL, and not to allow others to
+  use your version of this file under the terms of the MPL, indicate your
+  decision by deleting the provisions above and replace them with the notice
+  and other provisions required by the GPL or the LGPL. If you do not delete
+  the provisions above, a recipient may use your version of this file under
+  the terms of any one of the MPL, the GPL or the LGPL.
+ 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace OpenHardwareMonitor.Hardware.CPU {
+
+  internal sealed class AMD10CPU : AMDCPU {
+
+    private readonly Sensor coreTemperature;
+    private readonly Sensor[] coreClocks;
+    private readonly Sensor busClock;
+      
+    private const uint PERF_CTL_0 = 0xC0010000;
+    private const uint PERF_CTR_0 = 0xC0010004;
+    private const uint P_STATE_0 = 0xC0010064;
+    private const uint COFVID_STATUS = 0xC0010071;
+
+    private const byte MISCELLANEOUS_CONTROL_FUNCTION = 3;
+    private const ushort MISCELLANEOUS_CONTROL_DEVICE_ID = 0x1203;
+    private const uint REPORTED_TEMPERATURE_CONTROL_REGISTER = 0xA4;
+
+    private readonly uint miscellaneousControlAddress;
+
+    private double timeStampCounterMultiplier;
+
+    public AMD10CPU(int processorIndex, CPUID[][] cpuid, ISettings settings)
+      : base(processorIndex, cpuid, settings) 
+    {            
+      // AMD family 10h processors support only one temperature sensor
+      coreTemperature = new Sensor(
+        "Core" + (coreCount > 1 ? " #1 - #" + coreCount : ""), 0,
+        SensorType.Temperature, this, new [] {
+            new ParameterDescription("Offset [°C]", "Temperature offset.", 0)
+          }, settings);
+
+      // get the pci address for the Miscellaneous Control registers 
+      miscellaneousControlAddress = GetPciAddress(
+        MISCELLANEOUS_CONTROL_FUNCTION, MISCELLANEOUS_CONTROL_DEVICE_ID);
+
+      busClock = new Sensor("Bus Speed", 0, SensorType.Clock, this, settings);
+      coreClocks = new Sensor[coreCount];
+      for (int i = 0; i < coreClocks.Length; i++) {
+        coreClocks[i] = new Sensor(CoreString(i), i + 1, SensorType.Clock,
+          this, settings);
+        if (HasTimeStampCounter)
+          ActivateSensor(coreClocks[i]);
+      }
+
+      // set affinity to the first thread for all frequency estimations
+      IntPtr thread = NativeMethods.GetCurrentThread();
+      UIntPtr mask = NativeMethods.SetThreadAffinityMask(thread,
+        (UIntPtr)(1L << cpuid[0][0].Thread));
+
+      uint ctlEax, ctlEdx;
+      WinRing0.Rdmsr(PERF_CTL_0, out ctlEax, out ctlEdx);
+      uint ctrEax, ctrEdx;
+      WinRing0.Rdmsr(PERF_CTR_0, out ctrEax, out ctrEdx);
+
+      timeStampCounterMultiplier = estimateTimeStampCounterMultiplier();
+
+      // restore the performance counter registers
+      WinRing0.Wrmsr(PERF_CTL_0, ctlEax, ctlEdx);
+      WinRing0.Wrmsr(PERF_CTR_0, ctrEax, ctrEdx);
+
+      // restore the thread affinity.
+      NativeMethods.SetThreadAffinityMask(thread, mask);
+
+      Update();                   
+    }
+
+    private double estimateTimeStampCounterMultiplier() {
+      // preload the function
+      estimateTimeStampCounterMultiplier(0);
+      estimateTimeStampCounterMultiplier(0);
+
+      // estimate the multiplier
+      List<double> estimate = new List<double>(3);
+      for (int i = 0; i < 3; i++)
+        estimate.Add(estimateTimeStampCounterMultiplier(0.025));
+      estimate.Sort();
+      return estimate[1];
+    }
+
+    private double estimateTimeStampCounterMultiplier(double timeWindow) {
+      uint eax, edx;
+     
+      // select event "076h CPU Clocks not Halted" and enable the counter
+      WinRing0.Wrmsr(PERF_CTL_0,
+        (1 << 22) | // enable performance counter
+        (1 << 17) | // count events in user mode
+        (1 << 16) | // count events in operating-system mode
+        0x76, 0x00000000);
+
+      // set the counter to 0
+      WinRing0.Wrmsr(PERF_CTR_0, 0, 0);
+
+      long ticks = (long)(timeWindow * Stopwatch.Frequency);
+      uint lsbBegin, msbBegin, lsbEnd, msbEnd;
+
+      long timeBegin = Stopwatch.GetTimestamp() +
+        (long)Math.Ceiling(0.001 * ticks);
+      long timeEnd = timeBegin + ticks;
+      while (Stopwatch.GetTimestamp() < timeBegin) { }
+      WinRing0.Rdmsr(PERF_CTR_0, out lsbBegin, out msbBegin);
+      while (Stopwatch.GetTimestamp() < timeEnd) { }
+      WinRing0.Rdmsr(PERF_CTR_0, out lsbEnd, out msbEnd);
+
+      WinRing0.Rdmsr(COFVID_STATUS, out eax, out edx);
+      uint cpuDid = (eax >> 6) & 7;
+      uint cpuFid = eax & 0x1F;
+      double coreMultiplier = MultiplierFromIDs(cpuDid, cpuFid);
+
+      ulong countBegin = ((ulong)msbBegin << 32) | lsbBegin;
+      ulong countEnd = ((ulong)msbEnd << 32) | lsbEnd;
+
+      double coreFrequency = 1e-6 * 
+        (((double)(countEnd - countBegin)) * Stopwatch.Frequency) /
+        (timeEnd - timeBegin);
+
+      double busFrequency = coreFrequency / coreMultiplier;
+      return 0.5 * Math.Round(2 * TimeStampCounterFrequency / busFrequency);
+    }
+
+    protected override uint[] GetMSRs() {
+      return new uint[] { PERF_CTL_0, PERF_CTR_0, P_STATE_0, COFVID_STATUS };
+    }
+
+    public override string GetReport() {
+      StringBuilder r = new StringBuilder();
+      r.Append(base.GetReport());
+
+      r.Append("Miscellaneous Control Address: 0x");
+      r.AppendLine((miscellaneousControlAddress).ToString("X",
+        CultureInfo.InvariantCulture));
+      r.Append("Time Stamp Counter Multiplier: ");
+      r.AppendLine(timeStampCounterMultiplier.ToString(
+        CultureInfo.InvariantCulture));
+      r.AppendLine();
+
+      return r.ToString();
+    }
+
+    private static double MultiplierFromIDs(uint divisorID, uint frequencyID) {
+      return 0.5 * (frequencyID + 0x10) / (1 << (int)divisorID);
+    }
+
+    public override void Update() {
+      base.Update();
+
+      if (miscellaneousControlAddress != WinRing0.InvalidPciAddress) {
+        uint value;
+        if (WinRing0.ReadPciConfigDwordEx(miscellaneousControlAddress,
+          REPORTED_TEMPERATURE_CONTROL_REGISTER, out value)) {
+          coreTemperature.Value = ((value >> 21) & 0x7FF) / 8.0f +
+            coreTemperature.Parameters[0].Value;
+          ActivateSensor(coreTemperature);
+        } else {
+          DeactivateSensor(coreTemperature);
+        }
+      }
+
+      if (HasTimeStampCounter) {
+        double newBusClock = 0;
+
+        for (int i = 0; i < coreClocks.Length; i++) {
+          Thread.Sleep(1);
+
+          uint curEax, curEdx;
+          if (WinRing0.RdmsrTx(COFVID_STATUS, out curEax, out curEdx,
+            (UIntPtr)(1L << cpuid[i][0].Thread))) 
+          {
+            // 8:6 CpuDid: current core divisor ID
+            // 5:0 CpuFid: current core frequency ID
+            uint cpuDid = (curEax >> 6) & 7;
+            uint cpuFid = curEax & 0x1F;
+            double multiplier = MultiplierFromIDs(cpuDid, cpuFid);
+
+            coreClocks[i].Value = 
+              (float)(multiplier * TimeStampCounterFrequency / 
+              timeStampCounterMultiplier);
+            newBusClock = 
+              (float)(TimeStampCounterFrequency / timeStampCounterMultiplier);
+          } else {
+            coreClocks[i].Value = (float)TimeStampCounterFrequency;
+          }
+        }
+
+        if (newBusClock > 0) {
+          this.busClock.Value = (float)newBusClock;
+          ActivateSensor(this.busClock);
+        }
+      }
+    }
+
+    private static class NativeMethods {
+      private const string KERNEL = "kernel32.dll";
+
+      [DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
+      public static extern UIntPtr
+        SetThreadAffinityMask(IntPtr handle, UIntPtr mask);
+
+      [DllImport(KERNEL, CallingConvention = CallingConvention.Winapi)]
+      public static extern IntPtr GetCurrentThread();
+    }
+  }
+}
---
+++ tags/0.2.0/Hardware/CPU/IntelCPU.cs
@@ -0,0 +1,223 @@
+﻿/*
+  
+  Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+  The contents of this file are subject to the Mozilla Public License Version
+  1.1 (the "License"); you may not use this file except in compliance with
+  the License. You may obtain a copy of the License at
+ 
+  http://www.mozilla.org/MPL/
+
+  Software distributed under the License is distributed on an "AS IS" basis,
+  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+  for the specific language governing rights and limitations under the License.
+
+  The Original Code is the Open Hardware Monitor code.
+
+  The Initial Developer of the Original Code is 
+  Michael Möller <m.moeller@gmx.ch>.
+  Portions created by the Initial Developer are Copyright (C) 2009-2010
+  the Initial Developer. All Rights Reserved.
+
+  Contributor(s):
+
+  Alternatively, the contents of this file may be used under the terms of
+  either the GNU General Public License Version 2 or later (the "GPL"), or
+  the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+  in which case the provisions of the GPL or the LGPL are applicable instead
+  of those above. If you wish to allow use of your version of this file only
+  under the terms of either the GPL or the LGPL, and not to allow others to
+  use your version of this file under the terms of the MPL, indicate your
+  decision by deleting the provisions above and replace them with the notice
+  and other provisions required by the GPL or the LGPL. If you do not delete
+  the provisions above, a recipient may use your version of this file under
+  the terms of any one of the MPL, the GPL or the LGPL.
+ 
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace OpenHardwareMonitor.Hardware.CPU {
+  internal sealed class IntelCPU : GenericCPU {
+
+    private readonly Sensor[] coreTemperatures;
+    private readonly Sensor[] coreClocks;
+    private readonly Sensor busClock;
+
+    private readonly uint maxNehalemMultiplier;
+
+    private const uint IA32_THERM_STATUS_MSR = 0x019C;
+    private const uint IA32_TEMPERATURE_TARGET = 0x01A2;
+    private const uint IA32_PERF_STATUS = 0x0198;
+    private const uint MSR_PLATFORM_INFO = 0xCE;
+
+    private float[] Floats(float f) {
+      float[] result = new float[coreCount];
+      for (int i = 0; i < coreCount; i++)
+        result[i] = f;
+      return result;
+    }
+
+    public IntelCPU(int processorIndex, CPUID[][] cpuid, ISettings settings)
+      : base(processorIndex, cpuid, settings) 
+    {
+      float[] tjMax;
+      switch (family) {
+        case 0x06: {
+            switch (model) {
+              case 0x0F: // Intel Core (65nm)
+                switch (stepping) {
+                  case 0x06: // B2
+                    switch (coreCount) {
+                      case 2:
+                        tjMax = Floats(80 + 10); break;
+                      case 4:
+                        tjMax = Floats(90 + 10); break;
+                      default:
+                        tjMax = Floats(85 + 10); break;
+                    }
+                    tjMax = Floats(80 + 10); break;
+                  case 0x0B: // G0
+                    tjMax = Floats(90 + 10); break;
+                  case 0x0D: // M0
+                    tjMax = Floats(85 + 10); break;
+                  default:
+                    tjMax = Floats(85 + 10); break;
+                } break;
+              case 0x17: // Intel Core (45nm)
+                tjMax = Floats(100); break;
+              case 0x1C: // Intel Atom (45nm)
+                switch (stepping) {
+                  case 0x02: // C0
+                    tjMax = Floats(90); break;
+                  case 0x0A: // A0, B0
+                    tjMax = Floats(100); break;
+                  default:
+                    tjMax = Floats(90); break;
+                } break;
+              case 0x1A: // Intel Core i7 LGA1366 (45nm)
+              case 0x1E: // Intel Core i5, i7 LGA1156 (45nm)
+              case 0x25: // Intel Core i3, i5, i7 LGA1156 (32nm)
+              case 0x2C: // Intel Core i7 LGA1366 (32nm) 6 Core
+                uint eax, edx;
+                tjMax = new float[coreCount];
+                for (int i = 0; i < coreCount; i++) {
+                  if (WinRing0.RdmsrTx(IA32_TEMPERATURE_TARGET, out eax,
+                    out edx, (UIntPtr)(1L << cpuid[i][0].Thread))) {
+                    tjMax[i] = (eax >> 16) & 0xFF;
+                  } else {
+                    tjMax[i] = 100;
+                  }
+                }
+                if (WinRing0.Rdmsr(MSR_PLATFORM_INFO, out eax, out edx)) {
+                  maxNehalemMultiplier = (eax >> 8) & 0xff;
+                }
+                break;
+              default:
+                tjMax = Floats(100); break;
+            }
+          } break;
+        default: tjMax = Floats(100); break;
+      }
+
+      // check if processor supports a digital thermal sensor
+      if (cpuid[0][0].Data.GetLength(0) > 6 &&
+        (cpuid[0][0].Data[6, 0] & 1) != 0) {
+        coreTemperatures = new Sensor[coreCount];
+        for (int i = 0; i < coreTemperatures.Length; i++) {
+          coreTemperatures[i] = new Sensor(CoreString(i), i,
+            SensorType.Temperature, this, new [] { 
+              new ParameterDescription(
+                "TjMax [°C]", "TjMax temperature of the core.\n" + 
+                "Temperature = TjMax - TSlope * Value.", tjMax[i]), 
+              new ParameterDescription("TSlope [°C]", 
+                "Temperature slope of the digital thermal sensor.\n" + 
+                "Temperature = TjMax - TSlope * Value.", 1)}, settings);
+          ActivateSensor(coreTemperatures[i]);
+        }
+      } else {
+        coreTemperatures = new Sensor[0];
+      }
+
+      busClock = new Sensor("Bus Speed", 0, SensorType.Clock, this, settings);
+      coreClocks = new Sensor[coreCount];
+      for (int i = 0; i < coreClocks.Length; i++) {
+        coreClocks[i] =
+          new Sensor(CoreString(i), i + 1, SensorType.Clock, this, settings);
+        if (HasTimeStampCounter)
+          ActivateSensor(coreClocks[i]);
+      }
+
+      Update();
+    }
+
+    protected override uint[] GetMSRs() {
+      return new [] {
+        MSR_PLATFORM_INFO,
+        IA32_PERF_STATUS ,
+        IA32_THERM_STATUS_MSR,
+        IA32_TEMPERATURE_TARGET
+      };
+    }
+
+    public override void Update() {
+      base.Update();
+
+      for (int i = 0; i < coreTemperatures.Length; i++) {
+        uint eax, edx;
+        if (WinRing0.RdmsrTx(
+          IA32_THERM_STATUS_MSR, out eax, out edx,
+            (UIntPtr)(1L << cpuid[i][0].Thread))) {
+          // if reading is valid
+          if ((eax & 0x80000000) != 0) {
+            // get the dist from tjMax from bits 22:16
+            float deltaT = ((eax & 0x007F0000) >> 16);
+            float tjMax = coreTemperatures[i].Parameters[0].Value;
+            float tSlope = coreTemperatures[i].Parameters[1].Value;
+            coreTemperatures[i].Value = tjMax - tSlope * deltaT;
+          } else {
+            coreTemperatures[i].Value = null;
+          }
+        }
+      }
+
+      if (HasTimeStampCounter) {
+        double newBusClock = 0;
+        uint eax, edx;
+        for (int i = 0; i < coreClocks.Length; i++) {
+          System.Threading.Thread.Sleep(1);
+          if (WinRing0.RdmsrTx(IA32_PERF_STATUS, out eax, out edx,
+            (UIntPtr)(1L << cpuid[i][0].Thread))) {
+            if (maxNehalemMultiplier > 0) { // Core i3, i5, i7
+              uint nehalemMultiplier = eax & 0xff;
+              coreClocks[i].Value =(float)(nehalemMultiplier * 
+                TimeStampCounterFrequency / maxNehalemMultiplier);
+              newBusClock = 
+                (float)(TimeStampCounterFrequency / maxNehalemMultiplier);
+            } else { // Core 2
+              uint multiplier = (eax >> 8) & 0x1f;
+              uint maxMultiplier = (edx >> 8) & 0x1f;
+              // factor = multiplier * 2 to handle non integer multipliers 
+              uint factor = (multiplier << 1) | ((eax >> 14) & 1);
+              uint maxFactor = (maxMultiplier << 1) | ((edx >> 14) & 1);
+              if (maxFactor > 0) {
+                coreClocks[i].Value = 
+                  (float)(factor * TimeStampCounterFrequency / maxFactor);
+                newBusClock = 
+                  (float)(2 * TimeStampCounterFrequency / maxFactor);
+              }
+            }
+          } else { // Intel Pentium 4
+            // if IA32_PERF_STATUS is not available, assume TSC frequency
+            coreClocks[i].Value = (float)TimeStampCounterFrequency;
+          }
+        }
+        if (newBusClock > 0) {
+          this.busClock.Value = (float)newBusClock;
+          ActivateSensor(this.busClock);
+        }
+      }
+    }
+  }
+}
