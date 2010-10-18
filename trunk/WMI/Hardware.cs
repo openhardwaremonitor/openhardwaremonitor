cfec8292659c1272717fc02dd37d8dc2d6aa6394
paulwerelds <paulwerelds@319ec166-06d5-11df-9763-65e337b7a424>
2010-10-18 07:15:31 +0000

    Added a Parent property to the WMI exposed Hardware objects

--- trunk/WMI/Hardware.cs
+++ trunk/WMI/Hardware.cs
@@ -46,6 +46,7 @@ namespace OpenHardwareMonitor.WMI {
     public string HardwareType { get; private set; }
     public string Identifier { get; private set; }
     public string Name { get; private set; }
+    public string Parent { get; private set; }
 
     #endregion
 
@@ -53,6 +54,7 @@ namespace OpenHardwareMonitor.WMI {
       Name = hardware.Name;
       Identifier = hardware.Identifier.ToString();
       HardwareType = hardware.HardwareType.ToString();
+      Parent = hardware.Parent.Identifier.ToString();
     }
 
     public void Update() { }
