5bf3cf2f126446f1a86d1be8b03c1d173f7286a5
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2011-03-15 17:20:22 +0000

    Fixed Issue 172.

--- trunk/Properties/AssemblyVersion.cs
+++ trunk/Properties/AssemblyVersion.cs
@@ -37,5 +37,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.1.13")]
-[assembly: AssemblyInformationalVersion("0.2.1.13 Alpha")]
+[assembly: AssemblyVersion("0.2.1.14")]
+[assembly: AssemblyInformationalVersion("0.2.1.14 Alpha")]
