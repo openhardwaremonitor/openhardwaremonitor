ae81e8f22cb37aab12b13dd90e1f3ccb1291087c
Michael Möller <mmoeller@openhardwaremonitor.org>
2011-06-26 20:04:26 +0000

    Small correction for the mainboard specific configuration of the Gigabyte Z68X-UD7-B3 mainboard.

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -37,5 +37,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.2.3")]
-[assembly: AssemblyInformationalVersion("0.3.2.3 Alpha")]
+[assembly: AssemblyVersion("0.3.2.4")]
+[assembly: AssemblyInformationalVersion("0.3.2.4 Alpha")]
