938263fef213f4d673a274baa08241057d7d881d
Michael Möller <mmoeller@openhardwaremonitor.org>
2011-07-07 20:41:09 +0000

    Added support for reading the TAMG ACPI table on Gigabyte mainboards. Fixed a small problem with HDD/SSD names (0 chars are trimmed now as well).

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -37,5 +37,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.2.4")]
-[assembly: AssemblyInformationalVersion("0.3.2.4 Alpha")]
+[assembly: AssemblyVersion("0.3.2.5")]
+[assembly: AssemblyInformationalVersion("0.3.2.5 Alpha")]
