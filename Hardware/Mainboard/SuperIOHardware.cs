a53679c3adfbb690094e0d7b4ab11abdc428f9cc
Michael Möller <mmoeller@openhardwaremonitor.org>
2012-07-18 22:47:30 +0000

    Added a missing default case in the mainboard specific configuration selection. The missing default case prevented sensor data from getting displayed on unknown Asus mainboards with NCT6779D super I/O.

--- Hardware/Mainboard/SuperIOHardware.cs
+++ Hardware/Mainboard/SuperIOHardware.cs
@@ -1050,6 +1050,34 @@ namespace OpenHardwareMonitor.Hardware.Mainboard {
                   c.Add(new Ctrl("Chassis Fan #2", 2));
                   c.Add(new Ctrl("Chassis Fan #3", 3));
                   break;
+                default:
+                  v.Add(new Voltage("CPU VCore", 0));
+                  v.Add(new Voltage("Voltage #2", 1, true));
+                  v.Add(new Voltage("AVCC", 2, 34, 34));
+                  v.Add(new Voltage("3VCC", 3, 34, 34));
+                  v.Add(new Voltage("Voltage #5", 4, true));
+                  v.Add(new Voltage("Voltage #6", 5, true));
+                  v.Add(new Voltage("Voltage #7", 6, true));
+                  v.Add(new Voltage("3VSB", 7, 34, 34));
+                  v.Add(new Voltage("VBAT", 8, 34, 34));
+                  v.Add(new Voltage("VTT", 9));
+                  v.Add(new Voltage("Voltage #11", 10, true));
+                  v.Add(new Voltage("Voltage #12", 11, true));
+                  v.Add(new Voltage("Voltage #13", 12, true));
+                  v.Add(new Voltage("Voltage #14", 13, true));
+                  v.Add(new Voltage("Voltage #15", 14, true));
+                  t.Add(new Temperature("CPU Core", 0));
+                  t.Add(new Temperature("Temperature #1", 1));
+                  t.Add(new Temperature("Temperature #2", 2));
+                  t.Add(new Temperature("Temperature #3", 3));
+                  t.Add(new Temperature("Temperature #4", 4));
+                  t.Add(new Temperature("Temperature #5", 5));
+                  t.Add(new Temperature("Temperature #6", 6));
+                  for (int i = 0; i < superIO.Fans.Length; i++)
+                    f.Add(new Fan("Fan #" + (i + 1), i));
+                  for (int i = 0; i < superIO.Controls.Length; i++)
+                    c.Add(new Ctrl("Fan Control #" + (i + 1), i));
+                  break;
               }
               break;
             default:
