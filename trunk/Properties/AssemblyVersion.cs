28b21dc583f11bd9fdf72df4c8d479125372024a
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2012-07-18 22:47:30 +0000

    Added a missing default case in the mainboard specific configuration selection. The missing default case prevented sensor data from getting displayed on unknown Asus mainboards with NCT6779D super I/O.

--- trunk/Properties/AssemblyVersion.cs
+++ trunk/Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.0.19")]
-[assembly: AssemblyInformationalVersion("0.4.0.19 Alpha")]
+[assembly: AssemblyVersion("0.4.0.20")]
+[assembly: AssemblyInformationalVersion("0.4.0.20 Alpha")]
