b6ad3fa3dbe4f5e84434e59304fc7f622c911ed5
Michael Möller <mmoeller@openhardwaremonitor.org>
2012-10-01 17:10:09 +0000

    Changed a few types to internal (instead of public) in the Open Hardware Monitor library.

--- Hardware/HDD/DriveAttributeValue.cs
+++ Hardware/HDD/DriveAttributeValue.cs
@@ -4,7 +4,7 @@
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
  
-  Copyright (C) 2011 Michael Möller <mmoeller@openhardwaremonitor.org>
+  Copyright (C) 2011-2012 Michael Möller <mmoeller@openhardwaremonitor.org>
 	
 */
 
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 namespace OpenHardwareMonitor.Hardware.HDD {
 
   [StructLayout(LayoutKind.Sequential, Pack = 1)]
-  public struct DriveAttributeValue {
+  internal struct DriveAttributeValue {
     public byte Identifier;
     public short StatusFlags;
     public byte AttrValue;
