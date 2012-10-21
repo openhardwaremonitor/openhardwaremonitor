e64b1a506e966f32dfb4ecbf3d2822e22673fbab
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2012-10-21 14:45:24 +0000

    Enabled fan control on mainboards by default.

--- trunk/Properties/AssemblyVersion.cs
+++ trunk/Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1.5")]
-[assembly: AssemblyInformationalVersion("0.5.1.5 Alpha")]
+[assembly: AssemblyVersion("0.5.1.6")]
+[assembly: AssemblyInformationalVersion("0.5.1.6 Alpha")]
