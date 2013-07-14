b24637c47e9d0bfe325eac102ac66ed2e6a7e4d3
Michael Möller <mmoeller@openhardwaremonitor.org>
2013-07-14 16:32:27 +0000

    Updated the version.

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1.19")]
-[assembly: AssemblyInformationalVersion("0.5.1.19 Alpha")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyInformationalVersion("0.6.0 Beta")]
