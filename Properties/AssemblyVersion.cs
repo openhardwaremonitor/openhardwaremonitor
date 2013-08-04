5117c1292d7bc1fa4651e6fa6e70b557f29812e2
Michael Möller <mmoeller@openhardwaremonitor.org>
2013-08-04 06:54:46 +0000

    Fixed an issue in the NCT677X class causing a crash when calling Update.

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.0.6")]
-[assembly: AssemblyInformationalVersion("0.6.0.6 Alpha")]
+[assembly: AssemblyVersion("0.6.0.7")]
+[assembly: AssemblyInformationalVersion("0.6.0.7 Alpha")]
