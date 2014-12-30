5bc7020f0a300c67a2773cac2c9613db36b5e4ed
moel.mich <moel.mich@319ec166-06d5-11df-9763-65e337b7a424>
2014-12-30 22:49:32 +0000

    Fixed an OverflowException when trying to use (unsupported) 64-bit thread affinity masks on 32-bit systems.

--- trunk/Properties/AssemblyVersion.cs
+++ trunk/Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7.0 Beta")]
+[assembly: AssemblyVersion("0.7.1.0")]
+[assembly: AssemblyInformationalVersion("0.7.1 Beta")]
