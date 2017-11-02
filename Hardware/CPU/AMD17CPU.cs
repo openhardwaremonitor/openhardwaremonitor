5927e69c8a75160802ae73c6f2cbcf94a245e5bd
aquacomputer <info@aqua-computer.de>
2017-11-02 14:07:10 +0100

    Code Style

--- Hardware/CPU/AMD17CPU.cs
+++ Hardware/CPU/AMD17CPU.cs
@@ -1,8 +1,8 @@
-﻿//This Source Code Form is subject to the terms of the Mozilla Public
-//License, v. 2.0. If a copy of the MPL was not distributed with this
-//file, You can obtain one at http://mozilla.org/MPL/2.0/. 
-//Copyright (C) 2016-2017 Sebastian Grams <https://github.com/sebastian-dev>
-//Copyright (C) 2016-2017 Aqua Computer <https://github.com/aquacomputer, info@aqua-computer.de>
+﻿// This Source Code Form is subject to the terms of the Mozilla Public 
+// License, v. 2.0. If a copy of the MPL was not distributed with this 
+// file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+// Copyright (C) 2016-2017 Sebastian Grams <https://github.com/sebastian-dev> 
+// Copyright (C) 2016-2017 Aqua Computer <https://github.com/aquacomputer, info@aqua-computer.de> 
 
 using System;
 using System.Collections.Generic;
