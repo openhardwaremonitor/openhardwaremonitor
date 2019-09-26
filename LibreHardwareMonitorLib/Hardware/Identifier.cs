61454e1ae2f2ce18550bbb337719e39a3058884f
MoshiMoshi0 <902882+MoshiMoshi0@users.noreply.github.com>
2019-09-26 22:41:14 +0200

    Fix StackOverflowException when comparing Identifiers (#136)

--- LibreHardwareMonitorLib/Hardware/Identifier.cs
+++ LibreHardwareMonitorLib/Hardware/Identifier.cs
@@ -86,12 +86,11 @@ namespace LibreHardwareMonitor.Hardware
         }
 
         public static bool operator ==(Identifier id1, Identifier id2)
-        {
-            if (id1 == null && id2 == null)
+        { 
+            if (ReferenceEquals(id1, null) && ReferenceEquals(id2, null))
                 return true;
 
-
-            return id1 != null && id1.Equals(id2);
+            return !ReferenceEquals(id1, null) && id1.Equals(id2);
         }
 
         public static bool operator !=(Identifier id1, Identifier id2)
