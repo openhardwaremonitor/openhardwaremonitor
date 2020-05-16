39cfbc709d37706e78455aeb2a837d43d789129a
Michael Möller <mmoeller@openhardwaremonitor.org>
2020-05-16 22:43:05 +0200

    Changed the NCT679XD fan RPM calculation to use the 13-bit fan count register instead of the 16-bit RPM register.

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.3.1")]
-[assembly: AssemblyInformationalVersion("0.9.3.1 Alpha")]
+[assembly: AssemblyVersion("0.9.3.2")]
+[assembly: AssemblyInformationalVersion("0.9.3.2 Alpha")]
