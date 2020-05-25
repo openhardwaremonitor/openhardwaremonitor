5bd6bf0f3c95d6c83e6cbcbe0315a17eeeebfd45
Michael Möller <mmoeller@openhardwaremonitor.org>
2020-05-25 23:13:36 +0200

    Updated the version.

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyInformationalVersion("0.9.4")]
+[assembly: AssemblyVersion("0.9.5.0")]
+[assembly: AssemblyInformationalVersion("0.9.5")]
