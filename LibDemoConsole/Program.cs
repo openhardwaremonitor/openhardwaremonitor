a361fa1b22346de140bfc94dd4c52ae2bf93689f
Ttxman <seps.ladislav@seznam.cz>
2021-01-17 20:14:56 +0100

    Added minimal demo console application that enumerates HW using the MonitorLib

---
+++ LibDemoConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenHardwareMonitor.Hardware;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Computer c = new Computer()
+            {
+                CPUEnabled = true,
+                FanControllerEnabled = true,
+                GPUEnabled = true,
+                HDDEnabled = true,
+                MainboardEnabled = true,
+                RAMEnabled = true,
+            };
+            try
+            {
+                c.Open();
+                var visitor = new HWVisitor();
+                c.Accept(visitor);
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+    }
+}
