2002249f0232c240af141ad79151e15fedca9e6b
Cereal-Killa <pcfaguiar@gmail.com>
2021-06-04 15:48:05 -0300

    Support for parameters with values.

--- OpenHardwareMonitor/Utilities/StartupParams.cs
+++ OpenHardwareMonitor/Utilities/StartupParams.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 
 namespace OpenHardwareMonitor.Utilities {
   public class StartupParams {
-    private List<Types> Items;
+    private Dictionary<Types, string> Items;
 
     public StartupParams(string[] args) {
-      Items = new List<Types>();
+      Items = new Dictionary<Types, string>();
 
       for (int i = 0; i < args.Length; i++) {
         if (!args[i].StartsWith("--"))
           continue;
 
         Types type;
+        var parts = args[i].Split('=');
 
-        if (Enum.TryParse(args[i].ToUpper().TrimStart('-'), out type)) {
-          Items.Add(type);
+        if (Enum.TryParse(parts[0].ToUpper().TrimStart('-'), out type)) {
+          var value = parts.Length > 1 ? parts[1] : string.Empty;
+
+          if (Items.ContainsKey(type)) Items[type] = value;
+          else Items.Add(type, value);
         }
       }//for
     }
 
     public bool Contains(Types type) {
-      return Items.Contains(type);
+      return Items.ContainsKey(type);
+    }
+
+    public string GetValue(Types type) {
+      if (Contains(type)) return Items[type];
+      else return string.Empty;
     }
 
     public enum Types {
