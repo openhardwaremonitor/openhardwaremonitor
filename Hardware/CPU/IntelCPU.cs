3e68590a630ccaebb086c8fd9fc80182cbcbd269
DarkWeiTong <38516259+adsf130162@users.noreply.github.com>
2021-11-13 22:14:12 +0800

    Added Intel Core 12th CPU's enum.

--- Hardware/CPU/IntelCPU.cs
+++ Hardware/CPU/IntelCPU.cs
@@ -35,7 +35,8 @@ namespace OpenHardwareMonitor.Hardware.CPU {
       IceLake,
       CometLake,
       Tremont,
-      TigerLake
+      TigerLake,
+      AlderLake
     }
 
     private readonly Sensor[] coreTemperatures;
@@ -224,6 +225,10 @@ namespace OpenHardwareMonitor.Hardware.CPU {
                 microarchitecture = Microarchitecture.TigerLake;
                 tjMax = GetTjMaxFromMSR();
                 break;
+              case 0x97: //Intel Core 12th
+                microarchitecture = Microarchitecture.AlderLake;
+                tjMax = GetTjMaxFromMSR();
+                break;
               default:
                 microarchitecture = Microarchitecture.Unknown;
                 tjMax = Floats(100);
@@ -279,7 +284,8 @@ namespace OpenHardwareMonitor.Hardware.CPU {
         case Microarchitecture.IceLake:
         case Microarchitecture.CometLake:
         case Microarchitecture.Tremont:
-        case Microarchitecture.TigerLake: {
+        case Microarchitecture.TigerLake:
+        case Microarchitecture.AlderLake: {
             uint eax, edx;
             if (Ring0.Rdmsr(MSR_PLATFORM_INFO, out eax, out edx)) {
               timeStampCounterMultiplier = (eax >> 8) & 0xff;
@@ -350,7 +356,8 @@ namespace OpenHardwareMonitor.Hardware.CPU {
           microarchitecture == Microarchitecture.IceLake ||
           microarchitecture == Microarchitecture.CometLake ||
           microarchitecture == Microarchitecture.Tremont ||
-          microarchitecture == Microarchitecture.TigerLake) 
+          microarchitecture == Microarchitecture.TigerLake ||
+          microarchitecture == Microarchitecture.AlderLake) 
       {
         powerSensors = new Sensor[energyStatusMSRs.Length];
         lastEnergyTime = new DateTime[energyStatusMSRs.Length];
@@ -476,7 +483,8 @@ namespace OpenHardwareMonitor.Hardware.CPU {
               case Microarchitecture.IceLake:
               case Microarchitecture.CometLake:
               case Microarchitecture.Tremont:
-              case Microarchitecture.TigerLake: {
+              case Microarchitecture.TigerLake:
+              case Microarchitecture.AlderLake: {
                   uint multiplier = (eax >> 8) & 0xff;
                   coreClocks[i].Value = (float)(multiplier * newBusClock);
                 } break;
