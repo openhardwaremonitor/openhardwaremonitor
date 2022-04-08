216f2bf840f071b832a82c9ded5218672fea4363
Manuel Richter <manuel.richter@leica-geosystems.com>
2022-04-08 15:26:30 +0200

    Second instance check and formatting

--- OpenHardwareMonitor/Program.cs
+++ OpenHardwareMonitor/Program.cs
@@ -9,6 +9,7 @@
 */
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -21,100 +22,138 @@ using OpenHardwareMonitor.GUI;
 using OpenHardwareMonitor.Utilities;
 using OpenHardwareMonitorLib;
 
-namespace OpenHardwareMonitor {
-  public static class Program {
-    internal static CommandLineOptions Arguments;
-    [STAThread]
-    public static void Main(string[] args) {
-      if (!AllRequiredFilesAvailable())
-        Environment.Exit(0);
-
-      InstallAndConfigureNlog();
-
-      ParseCommandLine(args);
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      using (GUI.MainForm form = new GUI.MainForm()) {
-        form.FormClosed += delegate(Object sender, FormClosedEventArgs e) {
-          Application.Exit();
-        };
-        if (!Arguments.CloseAll)
+namespace OpenHardwareMonitor
+{
+    public static class Program
+    {
+        internal static CommandLineOptions Arguments;
+        [STAThread]
+        public static void Main(string[] args)
         {
-            Application.Run();
+            if (!AllRequiredFilesAvailable())
+                Environment.Exit(0);
+
+            if (CheckIfProcessExists())
+                Environment.Exit(0);
+
+            InstallAndConfigureNlog();
+
+            ParseCommandLine(args);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (GUI.MainForm form = new GUI.MainForm())
+            {
+                form.FormClosed += delegate (Object sender, FormClosedEventArgs e)
+                {
+                    Application.Exit();
+                };
+                if (!Arguments.CloseAll)
+                {
+                    Application.Run();
+                }
+                else
+                {
+                    form.ForceClose();
+                }
+            }
         }
-        else
+
+        private static void InstallAndConfigureNlog()
         {
-            form.ForceClose();
+            Logging.LoggerFactory = new ToNLogLoggerFactory();
+            Logging.LogInfo("Starting new session");
         }
-      }
+
+        /// <summary>
+        /// Checks if there is already an instance of Openhardwaremonitor running and brings up its window
+        /// in case its minimized or as icon in taskbar
+        /// </summary>
+        private static bool CheckIfProcessExists()
+        {
+            bool processExists = false;
+            Process thisInstance = Process.GetCurrentProcess();
+            if(Process.GetProcessesByName(thisInstance.ProcessName).Length > 1)
+            {
+                processExists = true;
+                using (var clientPipe = InterprocessCommunicationFactory.GetClientPipe())
+                {
+                    clientPipe.Connect();
+                    clientPipe.Write(new byte[] { (byte)SecondInstanceService.SecondInstanceRequest.MaximizeWindow }, 0, 1);
+                } 
+            }
+            return processExists;
+        }
+
+        private static void ParseCommandLine(string[] args)
+        {
+            StringBuilder helpText = new StringBuilder();
+            TextWriter helpWriter = new StringWriter(helpText);
+            CommandLine.Parser caseInsensitiveParser = new CommandLine.Parser(settings =>
+            {
+                settings.CaseSensitive = false;
+                settings.HelpWriter = helpWriter;
+                settings.EnableDashDash = CommandLine.Parser.Default.Settings.EnableDashDash;
+                settings.CaseInsensitiveEnumValues = CommandLine.Parser.Default.Settings.CaseInsensitiveEnumValues;
+                settings.IgnoreUnknownArguments = CommandLine.Parser.Default.Settings.IgnoreUnknownArguments;
+                settings.MaximumDisplayWidth = CommandLine.Parser.Default.Settings.MaximumDisplayWidth;
+                settings.ParsingCulture = CommandLine.Parser.Default.Settings.ParsingCulture;
+            });
+
+            var result = caseInsensitiveParser.ParseArguments<CommandLineOptions>(args);
+            Arguments = new CommandLineOptions(); // ensure it is not null, even if the command line parsing fails
+            result.WithParsed(x => Arguments = x);
+
+            // This writer is not empty if the (implicit) --help option was specified.
+            if (helpText.Length > 0)
+            {
+                helpWriter.WriteLine("Unknown options are silently ignored");
+                MessageBox.Show(helpWriter.ToString(), "Command line options", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private static bool IsFileAvailable(string fileName)
+        {
+            string path = Path.GetDirectoryName(Application.ExecutablePath) +
+              Path.DirectorySeparatorChar;
+
+            if (!File.Exists(path + fileName))
+            {
+                MessageBox.Show("The following file could not be found: " + fileName +
+                  "\nPlease extract all files from the archive.", "Error",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private static bool AllRequiredFilesAvailable()
+        {
+            if (!IsFileAvailable("Aga.Controls.dll"))
+                return false;
+            if (!IsFileAvailable("OpenHardwareMonitorLib.dll"))
+                return false;
+            if (!IsFileAvailable("OxyPlot.dll"))
+                return false;
+            if (!IsFileAvailable("OxyPlot.WindowsForms.dll"))
+                return false;
+
+            return true;
+        }
+
+        private sealed class ToNLogLoggerFactory : ILoggerFactory
+        {
+            public void Dispose()
+            {
+            }
+
+            public ILogger CreateLogger(string categoryName)
+            {
+                return new NLog.Extensions.Logging.NLogLoggerFactory().CreateLogger(categoryName);
+            }
+
+            public void AddProvider(ILoggerProvider provider) => throw new NotImplementedException();
+        }
     }
-
-    private static void InstallAndConfigureNlog() {
-      Logging.LoggerFactory = new ToNLogLoggerFactory();
-      Logging.LogInfo("Starting new session");
-    }
-
-    private static void ParseCommandLine(string[] args) {
-      StringBuilder helpText = new StringBuilder();
-      TextWriter helpWriter = new StringWriter(helpText);
-      CommandLine.Parser caseInsensitiveParser = new CommandLine.Parser(settings =>
-      {
-        settings.CaseSensitive = false;
-        settings.HelpWriter = helpWriter;
-        settings.EnableDashDash = CommandLine.Parser.Default.Settings.EnableDashDash;
-        settings.CaseInsensitiveEnumValues = CommandLine.Parser.Default.Settings.CaseInsensitiveEnumValues;
-        settings.IgnoreUnknownArguments = CommandLine.Parser.Default.Settings.IgnoreUnknownArguments;
-        settings.MaximumDisplayWidth = CommandLine.Parser.Default.Settings.MaximumDisplayWidth;
-        settings.ParsingCulture = CommandLine.Parser.Default.Settings.ParsingCulture;
-      });
-
-      var result = caseInsensitiveParser.ParseArguments<CommandLineOptions>(args);
-      Arguments = new CommandLineOptions(); // ensure it is not null, even if the command line parsing fails
-      result.WithParsed(x => Arguments = x);
-
-      // This writer is not empty if the (implicit) --help option was specified.
-      if (helpText.Length > 0) {
-        helpWriter.WriteLine("Unknown options are silently ignored");
-        MessageBox.Show(helpWriter.ToString(), "Command line options", MessageBoxButtons.OK);
-      }
-
-    }
-
-    private static bool IsFileAvailable(string fileName) {
-      string path = Path.GetDirectoryName(Application.ExecutablePath) +
-        Path.DirectorySeparatorChar;
-
-      if (!File.Exists(path + fileName)) {
-        MessageBox.Show("The following file could not be found: " + fileName + 
-          "\nPlease extract all files from the archive.", "Error",
-           MessageBoxButtons.OK, MessageBoxIcon.Error);
-        return false;
-      }
-      return true;      
-    }
-
-    private static bool AllRequiredFilesAvailable() {
-      if (!IsFileAvailable("Aga.Controls.dll"))
-        return false;
-      if (!IsFileAvailable("OpenHardwareMonitorLib.dll"))
-        return false;
-      if (!IsFileAvailable("OxyPlot.dll"))
-        return false;
-      if (!IsFileAvailable("OxyPlot.WindowsForms.dll"))
-        return false;
-      
-      return true;
-    }
-
-    private sealed class ToNLogLoggerFactory : ILoggerFactory {
-      public void Dispose() {
-      }
-
-      public ILogger CreateLogger(string categoryName) {
-        return new NLog.Extensions.Logging.NLogLoggerFactory().CreateLogger(categoryName);
-      }
-
-      public void AddProvider(ILoggerProvider provider) => throw new NotImplementedException();
-    }
-  }
 }
