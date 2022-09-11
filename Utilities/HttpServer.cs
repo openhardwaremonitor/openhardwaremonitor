6c7367f5163c41a36bc531f34e09b1e050e00143
Xinyu Chen <eng@kebugcheck.dev>
2022-09-11 13:57:21 -0700

    Upgrade to .NET Framework 4.8 and fix a minor bug

--- Utilities/HttpServer.cs
+++ Utilities/HttpServer.cs
@@ -78,9 +78,11 @@ namespace OpenHardwareMonitor.Utilities {
         return false;
 
       try {
-        listenerThread.Abort();
-        listener.Stop();
-        listenerThread = null;
+        if (listenerThread != null) {
+          listenerThread.Abort();
+          listener.Stop();
+          listenerThread = null;
+        }
       } catch (HttpListenerException) {
       } catch (ThreadAbortException) {
       } catch (NullReferenceException) {
